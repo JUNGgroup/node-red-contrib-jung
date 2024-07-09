880f70f97fd415acd335f0cd16ddf14cf04c881d
JUNGgroup <169043469+JUNGgroup@users.noreply.github.com>
2024-07-09 15:11:43 +0300

    Bugfix on copying existing Node, v2

--- jung-receive-status.js
+++ jung-receive-status.js
@@ -2,26 +2,6 @@ module.exports = async function (RED) {
   "use strict";
   const { got } = await import("got");
   var getBody = require("raw-body");
-  let gnode = {}; // will be required in callback
-
-  function generateUUID() {
-    // Public Domain/MIT
-    var d = new Date().getTime(); //Timestamp
-    var d2 = (typeof performance !== "undefined" && performance.now && performance.now() * 1000) || 0; //Time in microseconds since page-load or 0 if unsupported
-    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
-      var r = Math.random() * 16; //random number between 0 and 16
-      if (d > 0) {
-        // Use timestamp until depleted
-        r = (d + r) % 16 | 0;
-        d = Math.floor(d / 16);
-      } else {
-        // Use microseconds since page-load if supported
-        r = (d2 + r) % 16 | 0;
-        d2 = Math.floor(d2 / 16);
-      }
-      return (c === "x" ? r : (r & 0x3) | 0x8).toString(16);
-    });
-  }
 
   function createResponseWrapper(node, res) {
     var wrapper = {
@@ -248,69 +228,6 @@ module.exports = async function (RED) {
     return;
   }
 
-  function callback(req, res) {
-    // console.log("jung-receive-status> in callback");
-    // console.log("jung-receive-status> req:");
-    // console.log(req); // nes kitaip objekto neisskleidzia
-    // console.log(typeof req); // object
-    // console.log(typeof req.body); // string
-    // console.log(JSON.parse(req.body));   // object
-
-    var obody = [];
-    var sbody = "";
-    if (typeof req.body === "string") {
-      sbody = req.body;
-      obody = JSON.parse(req.body);
-    } else {
-      sbody = JSON.stringify(req.body);
-      obody = req.body;
-    }
-
-    var msgid = RED.util.generateId();
-    res._msgid = msgid;
-    // Since Node 15, req.headers are lazily computed and the property
-    // marked as non-enumerable.
-    // That means it doesn't show up in the Debug sidebar.
-    // This redefines the property causing it to be evaluated *and*
-    // marked as enumerable again.
-    Object.defineProperty(req, "headers", {
-      value: req.headers,
-      enumerable: true,
-    });
-
-    // debug
-    if (false) {
-      console.log("receive.js> in post. smsg received: " + sbody + ", type: " + typeof sbody);
-      gnode.warn("receive.js> in post. smsg received: " + sbody + ", type: " + typeof sbody);
-      // console.log("receive.js> in post. omsg received: " + obody + ", type: " + typeof obody);
-      // console.log('receive.js> in post. sbody.includes("jung-hosting.de/api/"): ' + sbody.includes("jung-hosting.de/api/"));
-      /* console.log(
-          'receive.js> in post. sbody.includes(\'"type":"datapoint"\'): ' +
-            sbody.replace(" ", "").includes('"type":"datapoint"')
-        ); */
-    }
-    if (sbody.includes("jung-hosting.de/api/") && sbody.replace(" ", "").includes('"type":"datapoint"')) {
-      // console.log("receive.js> ieskau datapointID=" + datapointID);
-      gnode.warn("receive.js> in post. msg received: " + sbody);
-      if (gnode.datapointID !== "") {
-        if (sbody.includes('"id":"' + gnode.datapointID + '"')) {
-          // console.log("receive.js> req.body=" + req.body);
-          // console.log("receive.js> req.body.data=" + req.body.data); // undefined
-          // console.log("receive.js> obody=" + JSON.stringify(obody));
-          const rez = obody.data[0].attributes.value;
-          // console.log("receive.js> radau, rez=" + JSON.stringify(rez));
-          // this.send({ _msgid: msgid, req: req, res: createResponseWrapper(node, res), payload: rez });
-          gnode.send({ _msgid: msgid, payload: rez });
-          res.sendStatus(204);
-          if (rez == "true") gnode.status({ fill: "green", shape: "dot", text: "on" });
-          if (rez == "false") gnode.status({ fill: "gray", shape: "ring", text: "off" });
-          return;
-        }
-      }
-    }
-    res.sendStatus(400);
-  }
-
   function nodeErrorHandler(err, req, res, next) {
     console.log(err);
     res.sendStatus(500);
@@ -353,8 +270,6 @@ module.exports = async function (RED) {
     node.datapointID = n.datapointID || "";
     node.subscriptionID = ""; // received from JUNG cloud
 
-    gnode = node;
-
     if (node.datapointID === "" || node.url === "") {
       console.log(node.name + " jung-receive-status> no JUNG datapoint ID, no URL provided, cancelling registration...");
       return;
@@ -365,6 +280,92 @@ module.exports = async function (RED) {
       console.log("node.datapointID: " + node.datapointID);
       console.log("node.id: " + node.id);
     }
+
+    //// end of setup, beginning function definition
+
+    function callback(req, res) {
+      // console.log("jung-receive-status> in callback");
+      // console.log("jung-receive-status> req:");
+      // console.log(req); // nes kitaip objekto neisskleidzia
+      // console.log(typeof req); // object
+      // console.log(typeof req.body); // string
+      // console.log(JSON.parse(req.body));   // object
+
+      var obody = [];
+      var sbody = "";
+      if (typeof req.body === "string") {
+        sbody = req.body;
+        obody = JSON.parse(req.body);
+      } else {
+        sbody = JSON.stringify(req.body);
+        obody = req.body;
+      }
+
+      var msgid = RED.util.generateId();
+      res._msgid = msgid;
+      // Since Node 15, req.headers are lazily computed and the property
+      // marked as non-enumerable.
+      // That means it doesn't show up in the Debug sidebar.
+      // This redefines the property causing it to be evaluated *and*
+      // marked as enumerable again.
+      Object.defineProperty(req, "headers", {
+        value: req.headers,
+        enumerable: true,
+      });
+
+      // debug
+      if (false) {
+        console.log("receive.js> in post. smsg received: " + sbody + ", type: " + typeof sbody);
+        node.warn("receive.js> in post. smsg received: " + sbody + ", type: " + typeof sbody);
+        // console.log("receive.js> in post. omsg received: " + obody + ", type: " + typeof obody);
+        // console.log('receive.js> in post. sbody.includes("jung-hosting.de/api/"): ' + sbody.includes("jung-hosting.de/api/"));
+        /* console.log(
+            'receive.js> in post. sbody.includes(\'"type":"datapoint"\'): ' +
+              sbody.replace(" ", "").includes('"type":"datapoint"')
+          ); */
+      }
+      if (sbody.includes("jung-hosting.de/api/") && sbody.replace(" ", "").includes('"type":"datapoint"')) {
+        // console.log("receive.js> ieskau datapointID=" + datapointID);
+        node.warn("receive.js> in post. msg received: " + sbody);
+        if (node.datapointID !== "") {
+          if (sbody.includes('"id":"' + node.datapointID + '"')) {
+            // console.log("receive.js> req.body=" + req.body);
+            // console.log("receive.js> req.body.data=" + req.body.data); // undefined
+            // console.log("receive.js> obody=" + JSON.stringify(obody));
+            const rez = obody.data[0].attributes.value;
+            // console.log("receive.js> radau, rez=" + JSON.stringify(rez));
+            // this.send({ _msgid: msgid, req: req, res: createResponseWrapper(node, res), payload: rez });
+            node.send({ _msgid: msgid, payload: rez });
+            res.sendStatus(204);
+            if (rez == "true") node.status({ fill: "green", shape: "dot", text: "on" });
+            if (rez == "false") node.status({ fill: "gray", shape: "ring", text: "off" });
+            return;
+          }
+        }
+      }
+      res.sendStatus(400);
+    }
+
+    this.on("close", function (done) {
+      var node = this;
+      if (node.subscriptionID !== "") {
+        // console.log("closing `" + this.name + "` with subscriptionID: " + node.subscriptionID);
+        unregisterFromJung(node);
+      }
+      console.log("waiting 1 seconds until everything closes...");
+      setTimeout(() => {
+        /* Code to run after 1 seconds */
+        // console.log("*** really finished ***");
+        done();
+      }, 1000);
+      RED.httpNode._router.stack.forEach(function (route, i, routes) {
+        if (route.route && route.route.path === node.url) {
+          routes.splice(i, 1);
+        }
+      });
+    });
+
+    //// end of function definition, beginning runtime
 
     registerOnJung(node);
     console.log("jung-receive-status> registered " + node.url + ", with subscriptionID " + this.subscriptionID);
@@ -394,25 +395,6 @@ module.exports = async function (RED) {
       });
       console.log("== routes end ==");
     }
-
-    this.on("close", function (done) {
-      var node = this;
-      if (node.subscriptionID !== "") {
-        // console.log("closing `" + this.name + "` with subscriptionID: " + node.subscriptionID);
-        unregisterFromJung(node);
-      }
-      console.log("waiting 1 seconds until everything closes...");
-      setTimeout(() => {
-        /* Code to run after 1 seconds */
-        // console.log("*** really finished ***");
-        done();
-      }, 1000);
-      RED.httpNode._router.stack.forEach(function (route, i, routes) {
-        if (route.route && route.route.path === node.url) {
-          routes.splice(i, 1);
-        }
-      });
-    });
   }
   RED.nodes.registerType("jung-receive-status", HTTPIn);
 };
